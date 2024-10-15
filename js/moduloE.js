a81619e2df7b974ea8abf82896e71bc47536ded9
Rosa <164552124+rosamrcl@users.noreply.github.com>
2024-10-15 15:17:39 -0300

    .

--- js/moduloE.js
+++ js/moduloE.js
@@ -1,13 +1,12 @@
 function contando(){
-    let ini=document.querySelector('#txti')
-    let fim=document.querySelector('#txtf')
-    let passo=document.querySelector('#txtp')
-    let res=document.querySelector('#res')
+    let ini=document.getElementById('txti')
+    let fim=document.getElementById('txtf')
+    let passo=document.getElementById('txtp')
+    let res=document.getElementById('res')
 
     if(ini.value.length==0 || fim.value.length==0 || passo.value.length==0){
         alert('[Erro] Faltam dados')
     }else{
-        alert('tudo ok') 
         res.innerHTML='Contando: '
         let i = Number(ini.value)
         let f = Number(fim.value)
