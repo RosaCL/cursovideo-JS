b04e8bbbf9fcf90646230c9dcf116f9ff71ba3c0
Rosa <costurezati@gmail.com>
2024-10-15 07:39:16 -0300

    .

---
+++ js/moduloE.js
@@ -0,0 +1,19 @@
+function contando(){
+    let ini=document.getElementById('texti')
+    let fim=document.getElementById('textf')
+    let passo=document.getElementById('textp')
+    let res=document.getElementById('res')
+
+    if(ini.value.length==0 || fim.value.length==0 || passo.value.length==0){
+        alert('[Erro] Faltam dados')
+    }else{
+        res.innerHTML='Contando: '
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        for(let c=1; c<=5; c+=p){
+            res.innerHTML+=`${c}`
+        }
+    }
+}
