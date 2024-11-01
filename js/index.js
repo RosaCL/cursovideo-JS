ca377bcd54118688096c14af7cbc53bc3b40d431
Rosa <costurezati@gmail.com>
2024-11-01 07:43:30 -0300

    .

---
+++ js/index.js
@@ -0,0 +1,14 @@
+document.getElementById("texto").innerHTML="Meu primeiro texto <b>JS!</b>!";
+document.write('Oi escrito com docmet.write');
+alert(10+5);
+console.log("Oi isso é um console.log");
+
+let a=5;b=6;c=7;
+
+function minhafuncao(){
+    document.write(5+5);
+    document.write(5+5);
+    document.write(5+5);
+    document.write(5+5);
+
+}
