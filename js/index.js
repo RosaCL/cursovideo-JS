10d971f79a5d41bd6d9e40e7673a9dfd581883c3
Rosa <costurezati@gmail.com>
2024-11-05 07:37:09 -0300

    .

--- js/index.js
+++ js/index.js
@@ -1,20 +1,26 @@
-document.getElementById("texto").innerHTML="Meu primeiro texto <b>JS!</b>!";
-document.write('Oi escrito com docmet.write');
-alert(10+5);
-console.log("Oi isso é um console.log");
+// document.getElementById("texto").innerHTML="Meu primeiro texto <b>JS!</b>!";
+// document.write('Oi escrito com docmet.write');
+// alert(10+5);
+// console.log("Oi isso é um console.log");
 
-let a=5;b=6;c=7;
+// let a=5;b=6;c=7;
 
-function minhafuncao(){
-    document.write(5+5);
-    document.write(5+5);
-    document.write(5+5);
-    document.write(5+5);
+// function minhafuncao(){
+//     document.write(5+5);
+//     document.write(5+5);
+//     document.write(5+5);
+//     document.write(5+5);
 
-}
+// }
 
-var nome, sobrenome, nomeCompleto;
-nome= "Rosa";
-sobrenome = "Chagas"
-nomeCompleto=nome+ " "+sobrenome;
-document.getElementById("texto").innerHTML=nomeCompleto;
+// var nome, sobrenome, nomeCompleto;
+// nome= "Rosa";
+// sobrenome = "Chagas"
+// nomeCompleto=nome+ " "+sobrenome;
+// document.getElementById("texto").innerHTML=nomeCompleto;
+
+var valor1,valor2,total;
+valor1=5;
+valor2=2;
+total=--valor1;
+alert (total);
